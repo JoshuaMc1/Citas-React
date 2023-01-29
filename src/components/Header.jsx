963bf6c3284cd0e23b1d1bb211d405d5aca85bca
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2023-01-28 23:49:26 -0600

    Cambios en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-      Seguimiento Pacientes
+      Seguimiento Pacientes Con GitHub
       <span className="text-emerald-600"> Veterinaria</span>
     </h1>
   );
